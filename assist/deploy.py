56c4d7db95b0ccaefde7877d7e7d699e0e1c4b29
tansongchen <tansongchen@pku.edu.cn>
2019-09-11 09:08:17 +0800

    删除无用文件

--- assist/deploy.py
+++ assist/deploy.py
@@ -2,6 +2,6 @@ import shutil
 import os
 
 addOnFolder = '/Users/tansongchen/Library/Application Support/Anki2/addons21/Evernote2Anki/'
-os.system('pyuic5 -o dialog.py dialog.ui')
-for file in ['__init__.py', 'dialog.py', 'dialog.ui']:
+os.system('pyuic5 -o ../dialog.py ../dialog.ui')
+for file in ['../__init__.py', '../dialog.py', '../dialog.ui']:
     shutil.copyfile(file, addOnFolder + file)
